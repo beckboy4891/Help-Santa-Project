520c9c314e15f0f58c9495f18e21eb42fc267a9e
beckboy4891 <monkeyboy4891@gmail.com>
2018-12-10 12:33:08 -0500

    Update SantaProj.java
    Basic File input

--- SantaProj.java
+++ SantaProj.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class SantaProj
 {
-	public static void main (String[] args){
-
+	public static void main (String[] args)throws IOException{
+		FileInputStream fs= new FileInputStream("kids.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(fs));
 
 	}
 }
