add6194f73e437d496174cf850246f214870f355
xD-Mig <45669573+xD-Mig@users.noreply.github.com>
2018-12-10 12:50:58 -0500

    Add files via upload

---
+++ Gifts.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Gifts{
+
+	public Scanner giftReader = new Scanner("gifts(1).txt");
+	ArrayList<String> name = new ArrayList<String>();
+	ArrayList<Integer> ageRange = new ArrayList<Integer>();
+	ArrayList<Integer> price = new ArrayList<Integer>();
+	ArrayList<Integer> buildDays = new ArrayList<Integer>();
+
+}
