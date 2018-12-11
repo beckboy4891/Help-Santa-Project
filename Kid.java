a8d3138606c48328808deb3ce3c3a960a75c2548
beckboy4891 <monkeyboy4891@gmail.com>
2018-12-11 12:22:01 -0500

    Update Kid.java

--- Kid.java
+++ Kid.java
@@ -1,23 +1,26 @@
-//initialize class, representing each kid gifts are being bought for
+//Made by Emily Bixler//
 public class Kid
 {
 	String name;
 	boolean nice;
 	int age;
 
-	public void Kid(String s, boolean n, int a)//initialize instance of "Kid", set all variables
+	public void Kid(String s, boolean n, int a)	//initialize instance of "Kid", set all variables//
 	{
 		name = s;
 		nice = n;
 		age = a;
 	}
-
-	public boolean isNice()//return true for nice, false for naughty
+	public boolean isNice()
+	{						//return true for nice, false for naughty//
 		return nice;
-
-	public int getAge()//return kid's age
+	}
+	public int getAge()	//return kid's age//
+	{
 		return age;
-
-	public String getName()//return kid's name
+	}
+	public String getName()	//return kid's name//
+	{
 		return name;
+	}
 }
