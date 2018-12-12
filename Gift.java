7bca65de9144bcf98c6b0d7588f1625424f5b69f
xoungdoogiez <45767885+xoungdoogiez@users.noreply.github.com>
2018-12-12 12:41:33 -0500

    Update Gift.java

--- Gift.java
+++ Gift.java
@@ -16,17 +16,27 @@ public class Gift
 	}
 
 	public int getMinAge()//return the minimum age for the gift//
+	{
 		return minAge;
+	}
 
 	public int getMaxAge()//return the maximum age for the gift//
+	{
 		return maxAge;
+	}
 
 	public int getTime()//return how long it takes to make the gift//
+	{
 		return timeToMake;
+	}
 
 	public String getName()//return kid's name//
+	{
 		return name;
+	}
 
 	public double getPrice()//return price of gift//
+	{
 		return price;
+	}
 }
