640d1c4719b6f41e6a7f609434ccf2622654ff8e
xoungdoogiez <45767885+xoungdoogiez@users.noreply.github.com>
2018-12-17 12:27:39 -0500

    Update Kid.java

--- Kid.java
+++ Kid.java
@@ -1,9 +1,21 @@
 //Made by Emily Bixler//
+import java.util.*;
+import java.io.*;
+
 public class Kid
 {
 	String name;
 	boolean nice;
 	int age;
+	double costSoFar = 0.0, costMax;
+	ArrayList<Gift> gifts = new ArrayList<>();
+
+	public Kid()
+	{
+		name = "";
+		nice = true;
+		age = 1;
+	}
 
 	public Kid(String s, boolean n, int a)	//initialize instance of "Kid", set all variables//
 	{
@@ -23,7 +35,14 @@ public class Kid
 	{
 		return name;
 	}
-	
+	public void addCost(double d)
+	{
+		costSoFar += d;
+	}
+	public double getCost()
+	{
+		return costSoFar;
+	}
 	public void printInfo()
 	{
 		System.out.println("Kid name: " + name);
@@ -32,5 +51,27 @@ public class Kid
 		else
 			System.out.println("Naughty");
 		System.out.println("Age: " + age);
+	}
+	public ArrayList<Gift> getGifts()
+	{
+		return gifts;
+	}
+	public void addGift(Gift g)
+	{
+		gifts.add(g);
+	}
+	public void setCostMax(double c)
+	{
+		if(nice)
+			costMax = c;
+
+		else
+			costMax = c / 2;
+
+		costMax *= 1.1;
+	}
+	public double getCostMax()
+	{
+		return costMax;
 	}
 }
